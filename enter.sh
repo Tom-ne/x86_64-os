1b4457e23b6a1042548a41233e7ed51a140aa9e0
Tom-ne <71755170+HeaG123@users.noreply.github.com>
2022-08-08 20:25:08 +0300

    changed text color

--- enter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo docker run --rm -it -v $(pwd):/root/env myos-buildenv
--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -2,6 +2,6 @@
 
 void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Welcome to the kernel!");
+    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+    print_str("Welcome to the kernel!");   
 }
